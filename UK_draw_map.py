bf89d639085f6e4890b4b0ddc1ee885f85b0a80a
mjguidry <m_guidry@msn.com>
2017-05-03 15:42:11 -0400

    Add NI to trace, first pass on draw_map script
    New trace adding Northern Ireland, first checkin on draw_map.py script
    (won't work yet because centroids file wasn't closed properly)

---
+++ UK_draw_map.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 01 12:51:35 2017
+
+@author: MGuidry
+"""
+
+from random import shuffle, sample
+from math import sqrt
+import numpy as np
+import colorsys
+from openpyxl import load_workbook
+import pickle
+import re
+from numpy import array, zeros
+from openpyxl import Workbook
+from openpyxl.utils import _get_column_letter
+from openpyxl.styles import Font, PatternFill,colors
+from openpyxl.comments import Comment
+
+execfile('_hungarian.py')
+
+def _get_colors(num_colors):
+    colors=[]
+    for i in np.arange(0., 360., 360. / num_colors):
+        hue = i/360.
+        lightness = (50 + np.random.rand() * 10)/100.
+        saturation = (90 + np.random.rand() * 10)/100.
+        (r,g,b)=colorsys.hls_to_rgb(hue, lightness, saturation)
+        colors.append('%02x%02x%02x' % (int(r*256), int(g*256), int(b*256)))
+    return colors
+    
+def dist_err(constituency,cell,gray_dict,centroids):
+    xmin=min([centroids[x][0] for x in centroids])
+    xmax=max([centroids[x][0] for x in centroids])
+    ymin=min([centroids[x][1] for x in centroids])
+    ymax=max([centroids[x][1] for x in centroids])
+    bbox=[xmin, ymin, xmax, ymax]
+    min_x=min([x[1] for x in gray_dict])
+    max_x=max([x[1] for x in gray_dict])+1
+    min_y=min([x[0] for x in gray_dict])
+    max_y=max([x[0] for x in gray_dict])+1
+    m_x=(bbox[2]-bbox[0])/(max_x-min_x)
+    b_x=bbox[2]-m_x*max_x
+    m_y=(bbox[3]-bbox[1])/(min_y-max_y)
+    b_y=bbox[3]-m_y*min_y    
+    x0=centroids[constituency][0]
+    y0=centroids[constituency][1]
+    x1=m_x*(cell[1]+0.5)+b_x
+    y1=m_y*(cell[0]+0.5)+b_y
+    #print x0,y0,x1,y1
+    tot_err=sqrt((x0-x1)**2+(y0-y1)**2)
+    return tot_err**4
+
+def isAdjacent(state,gray_dict):
+    blocks=[x for x in gray_dict if state in gray_dict[x]]
+    return_val=True
+    if (len(blocks)==1):
+        return_val=True
+    else:
+        for block in blocks:
+            if((block[0]-1,block[1]) in blocks or
+               (block[0]+1,block[1]) in blocks or
+               (block[0],block[1]-1) in blocks or
+               (block[0],block[1]+1) in blocks):
+                pass
+            else:
+                return_val=False
+    return return_val
+
+# First, get outline
+gray_dict=dict()
+wb_trace=load_workbook('UK_trace.xlsx',read_only=True)
+ws=wb_trace.active
+min_row=2
+max_row=58
+min_col=2
+max_col=36
+gray='FF808080'
+for row in range(min_row,max_row+1):
+    for col in range(min_col,max_col+1):
+        cell=ws.cell(row=row,column=col)
+        color=cell.style.fill.start_color.index
+        if(color==gray):
+            gray_dict[(row-min_row+1,col-min_col+1)]=''
+
+#Second, get constituency geographic data
+pkl_file=open('centroids.pkl','rb')
+centroids=pickle.load(pkl_file)
+pkl_file.close()
+constituency_keys=sorted(centroids.keys())
+cells=sorted(gray_dict.keys())
+
+constituency_arr=[]
+for constituency in constituency_keys:
+    constituency_arr.append(constituency)
+
+constituencies=sorted(set(constituency_arr))
+constituency_colors=_get_colors(len(constituencies))
+color_dict=dict()
+for k,constituency in enumerate(constituencies):
+    color_dict[constituency]=constituency_colors[k]
+
+constituency_keys=sorted(centroids.keys())
+
+#Optimize!
+print "Computing Cost Array"
+cost_arr=zeros((len(constituency_arr),len(constituency_arr)))
+for k,constituency in enumerate(constituency_arr):
+    for l,cell in enumerate(cells):
+        cost_arr[k,l]=dist_err(constituency,cell,gray_dict,centroids)
+
+print "Computing Best mapping"
+best_arr=linear_sum_assignment(cost_arr)
+for k in range(len(best_arr[0])):
+    constituency=constituency_arr[best_arr[0][k]]
+    gray_dict[cells[best_arr[1][k]]]=constituency
+
+adj=[isAdjacent(constituency,gray_dict) for constituency in constituencies]
+print 'Adjacent constituencies = '+str(len([x for x in adj if x==True]))
+print 'Non-adjacent constituencies = '+str(len([x for x in adj if x==False]))
+
+ft=Font('Arial',size=6)
+redFill = PatternFill(start_color=colors.RED,
+                   end_color=colors.RED,
+                   fill_type='solid')
+wb=Workbook()
+ws1=wb.active
+for cell in gray_dict:
+    ws1.cell(row=cell[0],column=cell[1]).value=gray_dict[cell]
+    comment = Comment(gray_dict[cell], 'Mike Guidry')
+    ws1.cell(row=cell[0],column=cell[1]).comment=comment
+    ws1.cell(row=cell[0],column=cell[1]).font=ft
+    constituency=gray_dict[cell]
+    fill = PatternFill(start_color=color_dict[constituency],
+                       end_color=color_dict[constituency],
+                       fill_type='solid')
+    ws1.cell(row=cell[0],column=cell[1]).fill=fill
+    ws1.column_dimensions[_get_column_letter(cell[1])].width=2.8
+    ws1.row_dimensions[cell[0]].height=20
+    
+wb.save(r'UK_block_map.xlsx')
--- UK_trace.py
+++ UK_trace.py
@@ -13,6 +13,8 @@ from openpyxl import Workbook
 from openpyxl.styles import PatternFill
 from openpyxl.cell import get_column_letter
 
+### England, Scotland, and Wales (minus two island constituencies)
+
 img=Image.open('./uk.gif')
 xsize=img.size[0]
 ysize=img.size[1]
@@ -106,5 +108,110 @@ for row in range(num_rows):
             #ss_function("setCellColor",[ws_name, sheet_name,row+2,col+2,"white"])
             #white_arr.append([row+2,col+2])
             ws.cell(row=row+2,column=col+2).fill=whiteFill
-            
+
+ws.cell(row=num_rows+3,column=1).value='# of blocks'
+ws.cell(row=num_rows+4,column=1).value=num_gray
+
+### Now, do Northern Ireland
+
+img=Image.open('./nireland.gif')
+xsize=img.size[0]
+ysize=img.size[1]
+mode="RGB"
+im=img.convert(mode)
+black=ImageColor.getcolor('black',mode)
+white=ImageColor.getcolor('white',mode)
+ImageDraw.floodfill(im,(xsize/2,ysize/2),black)
+#ImageDraw.floodfill(im,(xsize/2,ysize/4),black)
+#ImageDraw.floodfill(im,(xsize/2,3*ysize/4),black)
+#ImageDraw.floodfill(im,(75,345),black)
+pix=im.load()
+colors=im.getcolors(1024)
+num_black=[color for color in colors if color[1]==black][0][0]
+num_black=float(num_black)/21/21
+target_blocks=18
+num_gray=num_black
+
+
+sar_length=int(ceil(log(target_blocks)/log(2)))+1
+passed = 0
+times_thru =0
+new_xsize=xsize
+new_ysize=ysize
+
+
+threshold=0
+new_size=float(target_blocks)/num_gray
+new_xsize=int(round(new_xsize*sqrt(new_size)))
+new_ysize=int(round(new_ysize*sqrt(new_size)))
+print num_gray, new_xsize,new_ysize
+im2=im.resize((new_xsize,new_ysize))
+pix=im2.load()
+num_rows=int(ceil(new_ysize/21))
+num_cols=int(ceil(new_xsize/21))
+for k in range(sar_length):
+    threshold=threshold+0.5/2**k
+    num_gray=0
+    for row in range(num_rows):
+        for col in range(num_cols):
+            start_x=21*col
+            end_x=21*col+20
+            start_y=21*row
+            end_y=21*row+20
+            im_temp=im2.crop((start_x,start_y,end_x,end_y))
+            try:
+                colors_temp=im_temp.getcolors()
+                num_black_temp=[color for color in colors_temp if color[1]==black][0][0]
+                if(num_black_temp>21*21*threshold):
+                    num_gray=num_gray+1
+            except:
+                pass
+    print k, num_gray, threshold
+    if(num_gray<target_blocks):
+        last_under=k
+        last_under_threshold=threshold
+        last_under_cnt=num_gray
+        threshold=threshold-0.5/2**k
+
+num_gray=0
+sheets=wb.get_sheet_names()
+if('Northern Ireland' in sheets):
+    ws=wb.get_sheet_by_name('Northern Ireland')
+else:
+    ws=wb.create_sheet('Northern Ireland')
+grayFill=PatternFill(start_color='FF808080',
+                     end_color='FF808080',
+                     fill_type='solid')
+whiteFill=PatternFill(start_color='FFFFFFFF',
+                     end_color='FFFFFFFF',
+                     fill_type='solid')
+for row in range(num_rows):
+    ws.row_dimensions[row+2].height = 18
+    for col in range(num_cols):
+        ws.column_dimensions[get_column_letter((col+2))].width = 2.43
+        start_x=21*col
+        end_x=21*col+20
+        start_y=21*row
+        end_y=21*row+20
+        im_temp=im2.crop((start_x,start_y,end_x,end_y))
+        try:
+            colors_temp=im_temp.getcolors()
+            num_black_temp=[color for color in colors_temp if color[1]==black][0][0]
+            if(num_black_temp>21*21*threshold):
+                ws.cell(row=row+2,column=col+2).fill=grayFill
+                #ss_function("setCellGray",[ws_name, sheet_name,row+2,col+2])
+                #gray_arr.append([row+2,col+2])
+                num_gray=num_gray+1
+            else:
+                #ss_function("setCellColor",[ws_name, sheet_name,row+2,col+2,"white"])
+                ws.cell(row=row+2,column=col+2).fill=whiteFill
+                #white_arr.append([row+2,col+2])
+        except:
+            #ss_function("setCellColor",[ws_name, sheet_name,row+2,col+2,"white"])
+            #white_arr.append([row+2,col+2])
+            ws.cell(row=row+2,column=col+2).fill=whiteFill
+
+ws.cell(row=num_rows+3,column=1).value='# of blocks'
+ws.cell(row=num_rows+4,column=1).value=num_gray
+
 wb.save(filename = 'UK_trace.xlsx')
